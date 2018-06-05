5d079eb90f8a35a3f744f0bdd0eb99dbb1a297cc
davemflick <davemflick@yahoo.com>
2018-06-04 20:01:06 -0400

    imported dependencies

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <v-app>
+      <app-header></app-header>
+      <v-content>
+        <v-container fluid>
+          <router-view/>
+        </v-container>
+      </v-content>
+    </v-app>
   </div>
 </template>
 
 <script>
+import appHeader from './components/universal/Header.vue';
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    appHeader
+  }
 }
 </script>
 
