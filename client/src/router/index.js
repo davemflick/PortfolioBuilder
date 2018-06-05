5a6b69936a6cd8efe306a7696233462717736c89
davemflick <davemflick@yahoo.com>
2018-06-04 20:02:22 -0400

    home route bug fixes

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home.vue'
 
 Vue.use(Router)
 
 export default new Router({
+	mode: "history",
   routes: [
     {
       path: '/',
