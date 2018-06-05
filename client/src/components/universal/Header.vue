ca83a0cf2175d1bf3ae864f5a8f360bb4ea47658
davemflick <davemflick@yahoo.com>
2018-06-04 20:53:32 -0400

    nav bar adjustments

--- client/src/components/universal/Header.vue
+++ client/src/components/universal/Header.vue
@@ -1,26 +1,36 @@
 <template>
-	 <v-toolbar dark color="primary">
-    <v-toolbar-side-icon></v-toolbar-side-icon>
-    <v-toolbar-title class="white--text">Title</v-toolbar-title>
-    <v-spacer></v-spacer>
-    <v-btn icon>
-      <v-icon>search</v-icon>
-    </v-btn>
-    <v-btn icon>
-      <v-icon>apps</v-icon>
-    </v-btn>
-    <v-btn icon>
-      <v-icon>refresh</v-icon>
-    </v-btn>
-    <v-btn icon>
-      <v-icon>more_vert</v-icon>
-    </v-btn>
-  </v-toolbar>
+	<v-toolbar dark color="primary">
+		<v-toolbar-side-icon></v-toolbar-side-icon>
+		<v-toolbar-title class="white--text">
+			<router-link :to="{name: 'Home'}" tag="span">PortBuilder</router-link>
+		</v-toolbar-title>
+		<v-toolbar-title slot="extension" class="white--text" v-if="searchBar">
+			<v-text-field color="#fff" type="text" id="site-searcher" name="SearchSite" label="Search Site" ></v-text-field>
+		</v-toolbar-title>
+		<v-spacer></v-spacer>
+		<v-btn icon @click="searchBar = !searchBar">
+			<v-icon>search</v-icon>
+		</v-btn>
+		<v-toolbar-items  class="hidden-sm-and-down">
+			<v-btn icon>
+				<v-icon>more_vert</v-icon>
+			</v-btn>
+		</v-toolbar-items>
+	</v-toolbar>
 </template>
 
 <script>
+export default {
+	data(){
+		return {
+			searchBar: false
+		}
+	}
+}
 </script>
 
 <style scoped>
-
+ #site-searcher{
+ 	border-bottom: 1px solid #fff !important;
+ }
 </style>
