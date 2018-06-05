6d667ea50431b2d47c49679b591e204d2bb7be96
davemflick <davemflick@yahoo.com>
2018-06-04 20:01:30 -0400

    imported dependencies

--- client/src/main.js
+++ client/src/main.js
@@ -1,15 +1,20 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import { sync } from 'vuex-router-sync';
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css';
+import store from './store/store.js';
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+Vue.use(Vuetify)
+sync(store, router);
+
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
