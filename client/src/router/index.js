020324a1b09eae82d50d077ed18c9d23b105678b
davemflick <davemflick@yahoo.com>
2018-06-07 19:57:17 -0400

    login and signup links

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home.vue'
+import Login from '@/components/Login.vue'
+import SignUp from '@/components/SignUp.vue'
 
 Vue.use(Router)
 
@@ -11,6 +13,16 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+     {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/signup',
+      name: 'SignUp',
+      component: SignUp
     }
   ]
 })
