30d34b8f6a72bb116bfaf24aa7d2a75cab91da2b
davemflick <davemflick@yahoo.com>
2018-06-26 17:33:29 -0400

    password check

--- server/src/routes.js
+++ server/src/routes.js
@@ -4,6 +4,12 @@ var router = express.Router();
 //comment
 //POST -> Register
 router.post('/register', function(req, res, next){
+	if(req.body.password !== req.body.confirmPassword){
+		var err = new Error("Passwords do not match");
+		err.status = 400;
+		return next(err);
+	}
+
 	return res.json(req.body)
 });
 
@@ -12,6 +18,4 @@ router.post('/login', function(req, res, next){
 	return res.json(req.body)
 });
 
-module.exports = (router)=>{
-	router
-}
+module.exports = router
