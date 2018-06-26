68c5ffbb7bb6891e19c4088412bfdf950dbfda8d
davemflick <davemflick@yahoo.com>
2018-06-26 17:49:49 -0400

    registration post logic

--- server/src/routes.js
+++ server/src/routes.js
@@ -9,8 +9,23 @@ router.post('/register', function(req, res, next){
 		err.status = 400;
 		return next(err);
 	}
+	if(req.body.email && req.body.fname && req.body.lname){
+		let user = {
+			email: req.body.email,
+			name: {
+				first: req.body.fname,
+				last: req.body.lname
+			}
+		}
 
-	return res.json(req.body)
+		return res.json({Ok: true, user})
+	} else {
+		var err = new Error("Email and full name required");
+		err.status = 400;
+		err.details = {email: req.body.email, fname: req.body.fname, lname: req.body.lname}
+		return next(err);
+	}
+	
 });
 
 //POST -> login
