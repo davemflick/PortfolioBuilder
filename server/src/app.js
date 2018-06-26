31318333ea2f7ed06cbb3a042cd1d89c226b205e
davemflick <davemflick@yahoo.com>
2018-06-26 16:11:11 -0400

    mongoose

--- server/src/app.js
+++ server/src/app.js
@@ -2,14 +2,21 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const config = require('./config/config');
-
 const app = express();
+
+
+//set up mongodb / mongoose
+mongoose.connect("mongodb://localhost:27017/portbuilder");
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
 
 
 //app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
+require('./routes')(app); // pass in route to routes.js
 
 app.listen(config.port, ()=>{
 	console.log('Express Server Running')
