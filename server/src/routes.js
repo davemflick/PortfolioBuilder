62a4fec3ddd532242db60ae15118b7d7f34d842d
davemflick <davemflick@yahoo.com>
2018-06-26 16:11:48 -0400

    moved file

---
+++ server/src/routes.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+//comment
+//POST -> Register
+router.post('/register', function(req, res, next){
+	return res.json(req.body)
+});
+
+//POST -> login
+router.post('/login', function(req, res, next){
+	return res.json(req.body)
+});
+
+module.exports = (router)=>{
+	router
+}
