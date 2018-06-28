71aa7c2521dfeb3f79622f73f578b96de2e2f1a6
davemflick <davemflick@yahoo.com>
2018-06-27 20:11:14 -0400

    moved register callback to controller

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,28 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy.js');
+var AuthenticationController = require('./controllers/AuthenticationController.js');
 
 //comment
 //POST -> Register
-router.post('/register', AuthenticationControllerPolicy.register, function(req, res, next){
-	if(req.body.email && req.body.fname && req.body.lname){
-		let user = {
-			email: req.body.email,
-			name: {
-				first: req.body.fname,
-				last: req.body.lname
-			}
-		}
-
-		return res.json({Ok: true, user})
-	} else {
-		var err = new Error("Email and full name required");
-		err.status = 400;
-		err.details = {email: req.body.email, fname: req.body.fname, lname: req.body.lname}
-		return next(err);
-	}
-	
-});
+router.post('/register', AuthenticationControllerPolicy.register, AuthenticationController.register);
 
 //POST -> login
 router.post('/login', function(req, res, next){
