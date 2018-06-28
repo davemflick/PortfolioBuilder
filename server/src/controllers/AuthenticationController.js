7ca57deb462c35054e8600b509235149bcc73198
davemflick <davemflick@yahoo.com>
2018-06-27 20:09:55 -0400

    Auth Controller created

---
+++ server/src/controllers/AuthenticationController.js
@@ -0,0 +1,27 @@
+var User = require('../models/User.js');
+
+module.exports = {
+	async register(req, res, next){
+		if(req.body.email && req.body.fname && req.body.lname){
+			let userData = {
+				email: req.body.email,
+				name: {
+					first: req.body.fname,
+					last: req.body.lname
+				},
+				password: req.body.password
+			}
+			User.create(userData, function(err, user){
+				if(err){
+					return next(err);
+				}
+				return res.json({Ok: true, userData})
+			});
+		} else {
+			var err = new Error("Email and full name required");
+			err.status = 400;
+			err.details = {email: req.body.email, fname: req.body.fname, lname: req.body.lname}
+			return next(err);
+		}
+	}
+}
