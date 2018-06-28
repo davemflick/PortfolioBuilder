a88a2e146753a60d22638ede57da7288ab010b09
davemflick <davemflick@yahoo.com>
2018-06-28 18:44:24 -0400

    login controller

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -16,6 +16,7 @@ module.exports = {
 				if(err){
 					return next(err);
 				}
+				req.session.userId = user._id;
 				return res.json({Ok: true, Msg: 'User successfuly created'})
 			});
 		} else {
@@ -24,5 +25,28 @@ module.exports = {
 			err.details = {email: req.body.email, fname: req.body.fname, lname: req.body.lname}
 			return next(err);
 		}
+	},
+
+	async login(req, res, next){
+		if(!req.body.email || !req.body.password){
+			let error = new Error("Email and Password Required");
+			error.status = 400;
+			error.details = {};
+			return next(err);
+		}
+		User.authenticate(req.body.email, req.body.password, (err, user)=>{
+			if(err){
+				let error = new Error("Invalid Credentials");
+				error.status = 401;
+				error.details = {};
+				return next(err);
+			}
+			req.session.userId = user._id;
+			var userReturn = {
+				email: user.email,
+				name: {first: user.name.first, last: user.name.last}
+			}
+			return res.json({Ok: true, Msg: 'Successful user login', User: userReturn})
+		})
 	}
 }
