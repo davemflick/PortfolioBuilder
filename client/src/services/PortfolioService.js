7da47188826c0f045d5bec7053b2809bfef1a7a6
davemflick <davemflick@yahoo.com>
2018-07-04 09:55:55 -0400

    update portfolio call

--- client/src/services/PortfolioService.js
+++ client/src/services/PortfolioService.js
@@ -3,5 +3,9 @@ import Api from '@/services/Api';
 export default {
 	findPortfolio(req){
 		return Api().get(`portfolio/${req}`)// Get user
+	},
+
+	updatePortfolio(id, req){
+		return Api().put(`portfolio/update/${id}`, req)
 	}
 }
