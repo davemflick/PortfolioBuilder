34166b943ec88f807234bcb41565b79393e1b1b7
davemflick <davemflick@yahoo.com>
2018-07-04 10:30:40 -0400

    added build stage to state

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -9,17 +9,27 @@ export default new Vuex.Store({
 	plugins: [createPersistedState()],
 	state: {
 		user: null,
-		userLoggedIn: false
+		userLoggedIn: false,
+		buildStage: null
+	},
+	getters:{
+		getBuildStage(state){return state.buildStage;}
 	},
 	mutations:{
 		setUser(state, user){
 			state.user = user;
 			state.user ? state.userLoggedIn = true : state.userLoggedIn = false
+		},
+		setBuildStage(state, stage){
+			state.buildStage = stage;
 		}
 	},
 	actions:{
 		setUser({commit}, user){
 			commit('setUser', user)
+		},
+		setBuildStage({commit}, stage){
+			commit('setBuildStage', stage);
 		}
 	}
 })
