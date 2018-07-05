ce5f10156116a23d4df709f6452536aa444418f0
davemflick <davemflick@yahoo.com>
2018-07-05 17:57:53 -0400

    pull in portfolio data

--- client/src/components/Portfolio.vue
+++ client/src/components/Portfolio.vue
@@ -1,15 +1,31 @@
 <template>
-	<h1>Hello </h1>
+	<v-container>
+		<v-layout wrap v-if="portfolioUser && portfolio">
+			<v-flex xs12 sm4>
+				<v-avatar :size="150" :tile="true">
+					<img :src="defaultProfileImg" />
+				</v-avatar>
+			</v-flex>
+			<v-flex xs12 sm8>
+				<h2>{{ portfolioUser.name.first }} {{ portfolioUser.name.last }}</h2>
+				<p>{{ portfolio.aboutUser }}</p>
+			</v-flex>
+		</v-layout>
+	</v-container>
 </template>
 
 <script>
 	import PortfolioService from '@/services/PortfolioService';
 	import appFormPanel from './universal/FormPanel.vue';
+	import defaultProfileImg from '@/assets/emptyProfile.png';
 
 	export default{
 		data(){
 			return {
-				user: null
+				loggedInUser: null,
+				portfolio: null,
+				portfolioUser: null,
+				defaultProfileImg: defaultProfileImg
 			}
 		},
 		async mounted(){
@@ -19,9 +35,26 @@
 				return;
 			}
 			if(this.$store.state.userLoggedIn){
-				this.user = this.$store.state.user;
+				this.loggedInUser = this.$store.state.user;
 			}
-			const portfolio = await PortfolioService.findPortfolio(username);
+			try{
+				const portfolio = await PortfolioService.findPortfolio(username);
+				if(!portfolio.data.portfolio){
+					console.log('Portfolio not found');
+				} else {
+					this.portfolio = portfolio.data.portfolio;
+				}
+				if(!portfolio.data.user){
+					console.log('Portfolio User not found');
+				} else {
+					this.portfolioUser = portfolio.data.user;
+				}
+				console.log(portfolio);
+			}catch(error){
+				if(error){
+					console.log(error);
+				}
+			}
 		}
 	}
 </script>
