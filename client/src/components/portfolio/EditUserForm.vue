86a8fc4a5cdfb4dda939ba2e48239aed34e8c8bb
davemflick <davemflick@yahoo.com>
2018-07-08 11:11:46 -0400

    handeling change to user where vuex is being a bitch

--- client/src/components/portfolio/EditUserForm.vue
+++ client/src/components/portfolio/EditUserForm.vue
@@ -1,23 +1,26 @@
 <template>
-    <app-form-panel title="User Information" v-if="user">
-      <form id="edit-user-form">
-        <v-layout wrap>
-          <v-flex xs12 sm6 pr-1>
-            <v-text-field type="text" label="First Name" v-model="user.name.first"></v-text-field>
-          </v-flex>
-          <v-flex xs12 sm6 pl-1>
-            <v-text-field type="text" label="Last Name" v-model="user.name.last"></v-text-field>
-          </v-flex>
-        </v-layout>
-        <v-layout wrap>
-          <v-flex xs12 sm6 pr-1>
-            <v-text-field type="text" label="Username" v-model="user.username"></v-text-field>
-          </v-flex>
-          <v-flex xs12 sm6 pr-1>
-            <v-text-field type="email" label="Email" v-model="user.email"></v-text-field>
-          </v-flex>
-        </v-layout>
+  <app-form-panel title="User Information" v-if="user" >
+    <form id="edit-user-form">
+      <v-layout wrap>
+        <v-flex xs12 sm6 pr-1>
+          <v-text-field type="text" label="First Name" v-model="user.name.first"></v-text-field>
+        </v-flex>
+        <v-flex xs12 sm6 pl-1>
+          <v-text-field type="text" label="Last Name" v-model="user.name.last"></v-text-field>
+        </v-flex>
       </v-layout>
+      <v-layout wrap>
+        <v-flex xs12 sm6 pr-1>
+          <v-text-field type="text" label="Username" v-model="user.username"></v-text-field>
+        </v-flex>
+        <v-flex xs12 sm6 pr-1>
+          <v-text-field type="email" label="Email" v-model="user.email"></v-text-field>
+        </v-flex>
+      </v-layout>
+      <br>
+      <div v-if="error" class="error-alert">{{ error }}</div>
+      <br>
+      <v-btn dark color="primary" @click="updateUser">Update</v-btn>
     </form>
   </app-form-panel>
 </template>
@@ -25,15 +28,26 @@
 <script>
 
   import appFormPanel from '../universal/FormPanel.vue';
+  import _ from 'lodash';
   export default {
-    props: ['user'],
+    data(){
+      return {
+        error: null,
+        user: _.cloneDeep(this.$store.state.user)
+      }
+    },
     components:{
       appFormPanel
+    },
+    methods:{
+      updateUser(){
+        console.log(this.user);
+      }
     }
   }
-</script>
+  </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
 
 </style>
