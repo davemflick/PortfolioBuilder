6be7fa488b57f0bb1c60fa72f04bf5109a7467b8
davemflick <davemflick@yahoo.com>
2018-07-14 10:21:19 -0400

    add vue draggable resizeable component

--- client/src/components/universal/FileUploader.vue
+++ client/src/components/universal/FileUploader.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="white elevation-2">
+    <div style="height: 500px; width: 500px; border: 1px solid red; position: relative;">
+      <vue-draggable-resizable  :active="true" :w="100" :h="100" v-on:dragging="onDrag" v-on:resizing="onResize" :parent="true">
+        <p>Hello! I'm a flexible component. You can drag me around and you can resize me.<br>
+        X: {{ x }} / Y: {{ y }} - Width: {{ width }} / Height: {{ height }}</p>
+      </vue-draggable-resizable>
+    </div>
+    <br>
+    <br>
     <form id="upload-form">
       <v-layout wrap>
         <v-flex xs12>
@@ -23,10 +31,24 @@
       return{
         imgName: null,
         imgUrl: null,
-        imgFile: null
+        imgFile: null,
+        width: 0,
+        height: 0,
+        x: 0,
+        y: 0
       }
     },
     methods:{
+      onResize: function (x, y, width, height) {
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+      },
+      onDrag: function (x, y) {
+        this.x = x
+        this.y = y
+      },
       pickFile(){
         this.$refs.image.click();
       },
