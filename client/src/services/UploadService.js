d58822f1edb23a50a94b228d8e3d0bf934ade65f
davemflick <davemflick@yahoo.com>
2018-07-15 09:25:11 -0400

    pass boundaries into headers

--- client/src/services/UploadService.js
+++ client/src/services/UploadService.js
@@ -1,12 +1,13 @@
 import Api from '@/services/Api';
 
 export default {
-	UploadUserProfileImage(req){
-		return Api().post(`user/upload/img`, req,{
+	UploadUserProfileImage(file, bounds){
+		return Api().post(`user/upload/img`, file, {
 			headers: {
 				'accept': 'application/json',
 				'Accept-Language': 'en-US,en;q=0.8',
-				'Content-Type': `multipart/form-data; boundary=${req._boundary}`,
+				'Boundaries': bounds, 
+				'Content-Type': `multipart/form-data; boundary=${file._boundary}`,
 			}
 		})// Get user
 	}
