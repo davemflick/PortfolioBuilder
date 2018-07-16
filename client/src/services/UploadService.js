b3210601de0c796ca4fb0fb156f56fa265d5b075
davemflick <davemflick@yahoo.com>
2018-07-16 19:10:15 -0400

    pass in db target data as well

--- client/src/services/UploadService.js
+++ client/src/services/UploadService.js
@@ -1,12 +1,13 @@
 import Api from '@/services/Api';
 
 export default {
-	UploadUserProfileImage(file, bounds){
+	UploadUserProfileImage(file, bounds, dbData){
 		return Api().post(`user/upload/img`, file, {
 			headers: {
 				'accept': 'application/json',
 				'Accept-Language': 'en-US,en;q=0.8',
-				'Boundaries': bounds, 
+				'Boundaries': bounds,
+				'dbData': dbData,
 				'Content-Type': `multipart/form-data; boundary=${file._boundary}`,
 			}
 		})// Get user
