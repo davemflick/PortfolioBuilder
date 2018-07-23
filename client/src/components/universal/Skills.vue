53123d54bc78fab82b2ab7dc315ba303a882bf2b
davemflick <davemflick@yahoo.com>
2018-07-22 20:17:32 -0400

    style up results and pull icons

--- client/src/components/universal/Skills.vue
+++ client/src/components/universal/Skills.vue
@@ -1,7 +1,7 @@
 <template>
   <app-form-panel :title="'Skills'">
     <v-layout wrap>
-      <v-flex xs-12 class="p-2">
+      <v-flex xs12 class="p-2">
         <v-text-field
         hide-details
         prepend-icon="search"
@@ -11,12 +11,37 @@
         ></v-text-field>
       </v-flex>
     </v-layout>
-    <v-layout v-if="searchResults.length > 0">
-      <v-flex xs-6 sm-3 v-for="skill in searchResults" :key="skill.name">
-        <p>{{ skill.name }}</p>
-      </v-flex>
-    </v-layout>
-  </app-form-panel>
+    <div v-if="searchResults.length > 0">
+      <h2>
+        Results <small>brought to you by <a href="http://konpa.github.io/devicon/" target="_blank" title="Devicon">Devicon</a></small>
+      </h2>
+      <v-layout column justify-space-between>
+        <v-flex xs12 v-for="skill in searchResults" :key="skill.name">
+          <h5 class="results-type">{{ skill.name }}</h5>
+          <v-layout wrap justify-space-between>
+            <v-flex sm6 class="text-xs-center" v-for="icon in skill.versions.font" :key="icon">
+              <v-layout wrap justify-space-between>
+                <v-flex sm6 class="pa-2">
+                  <div class="pa-1 single-icon">
+                    <i :class="`devicon-icon devicon-${skill.name}-${icon}`"></i>
+                    <br>
+                    <p>{{ icon }}</p>
+                  </div>
+                </v-flex>
+                <v-flex sm6 class="pa-2">
+                  <div class=" pa-1 single-icon">
+                   <i :class="`devicon-icon devicon-${skill.name}-${icon} colored`"></i>
+                   <br>
+                   <p>{{ icon }} with color</p>
+                 </div>
+               </v-flex>
+             </v-layout>
+           </v-flex>
+         </v-layout>
+       </v-flex>
+     </v-layout>
+   </div>
+ </app-form-panel>
 </template>
 
 <script>
@@ -36,7 +61,7 @@
     methods: {
       createSearchResults(){
         if(this.searchTerm){
-          if(this.searchTerm.length > 1){
+          if(this.searchTerm.length >= 1){
             this.searchResults = this.allSkills.filter(skill=> skill.name.includes(this.searchTerm));
           } else {
             this.searchResults = [];
@@ -51,5 +76,17 @@
 </script>
 
 <style scoped>
+.results-type{
+  font-size: 1.25em;
+  margin-bottom: 10px;
+}
+.single-icon{
+  border: 1px solid #ccc;
+  border-radius: 3px;
+}
 
+h2 small{
+  font-size: 11px;
+  color: #aaa;
+}
 </style>
