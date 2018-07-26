249ffa83c50b74f8255615761a655022a8c5e7bb
davemflick <davemflick@yahoo.com>
2018-07-26 17:51:41 -0400

    refactor and handling bad load data

--- client/src/components/portfolio/Portfolio.vue
+++ client/src/components/portfolio/Portfolio.vue
@@ -1,10 +1,11 @@
 <template>
 	<v-container>
+		<v-progress-linear v-show="!pageLoaded" :indeterminate="true"></v-progress-linear>
 		<v-layout wrap v-if="portfolioUser && portfolio">
 			<v-flex xs12 sm4>
 				<v-avatar :size="150" :tile="true">
 					<img v-if="portfolio.profilePicture.length > 0"
-							 :src="'http://localhost:8081/' + portfolio.profilePicture.find((p)=>{return p.isMain}).path" />
+					:src="'http://localhost:8081/' + portfolio.profilePicture.find((p)=>{return p.isMain}).path" />
 					<img v-else :src="defaultProfileImg" />
 				</v-avatar>
 			</v-flex>
@@ -13,6 +14,10 @@
 				<p>{{ portfolio.aboutUser }}</p>
 			</v-flex>
 		</v-layout>
+		<h1 v-show="noPortfolioFound.status" class="text-xs-center">
+			{{ noPortfolioFound.msg }}<br>
+			Go <router-link tag="a" to="/"><a title="Go Home">Home</a></router-link>
+		</h1>
 	</v-container>
 </template>
 
@@ -27,11 +32,24 @@
 				loggedInUser: null,
 				portfolio: null,
 				portfolioUser: null,
-				defaultProfileImg: defaultProfileImg
+				defaultProfileImg: defaultProfileImg,
+				noPortfolioFound: {
+					status: false,
+					msg: null
+				},
+				pageLoaded: false
+			}
+		},
+		methods:{
+			setNoPortfolioMsg(msg){
+				this.pageLoaded = true;
+				this.noPortfolioFound.status = true;
+				this.noPortfolioFound.msg = msg;
 			}
 		},
 		async mounted(){
-			const username= this.$store.state.route.params.username
+			const username = this.$store.state.route.params.username
+			//If route does not contain a username parameter
 			if(!username){
 				this.$router.push({name: 'Home'});
 				return;
@@ -39,20 +57,32 @@
 			if(this.$store.state.userLoggedIn){
 				this.loggedInUser = this.$store.state.user;
 			}
+
 			try{
 				const portfolio = await PortfolioService.findPortfolio(username);
+				//Check to make sure a portfolio associated to username is found
 				if(!portfolio.data.portfolio){
 					console.log('Portfolio not found');
+					this.setNoPortfolioMsg("Portfolio not found.");
+					return;
 				} else {
+					//Make sure portfolio is active
 					if(!portfolio.data.portfolio.isActive){
-						this.$router.push({name: 'Home'});
+						this.setNoPortfolioMsg("User has deactivated their portfolio.");
+						return;
+					} else {
+						this.portfolio = portfolio.data.portfolio;
 					}
-					this.portfolio = portfolio.data.portfolio;
 				}
+				//Check to make sure a user associated to username is found
 				if(!portfolio.data.user){
-					console.log('Portfolio User not found');
+					this.setNoPortfolioMsg("User account not found.")
 				} else {
 					this.portfolioUser = portfolio.data.user;
+				}
+				//Stop page loader if both a portfolio and user are found
+				if(this.portfolio && this.portfolioUser){
+					this.pageLoaded = true;
 				}
 				console.log(portfolio);
 			}catch(error){
