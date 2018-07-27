b0cc807eedcbcfef5bcdb3ca267317d518d229a0
davemflick <davemflick@yahoo.com>
2018-07-26 20:05:11 -0400

    moved build components here

---
+++ client/src/components/portfolio/build/PortfolioBuild.vue
@@ -0,0 +1,95 @@
+<template>
+	<div>
+		<v-jumbotron>
+			<v-container fill-height>
+				<v-layout align-center text-xs-center>
+					<v-flex>
+						<h3 class="display-3">Welcome to PortBuilder<span v-if="user"> {{ user.name.first }}</span>!</h3>
+						<p>
+							Let's get started by creating your profile.
+						</p>
+						<p class='error-alert' v-if="error">
+							{{ error }}
+						</p>
+					</v-flex>
+				</v-layout>
+			</v-container>
+		</v-jumbotron>
+		<v-stepper v-model="stage" vertical>
+			<v-stepper-step :complete="stage > 1" step="1">
+				Basic Information
+			</v-stepper-step>
+
+			<v-stepper-content step="1">
+				<v-card color="grey lighten-1" class="mb-5">
+					<app-stage-one :portfolioId="portfolioId" :profilePictures="portfolioPictures" v-on:next="nextStage"></app-stage-one>
+				</v-card>
+			</v-stepper-content>
+
+			<v-stepper-step :complete="stage > 2" step="2">Add a Project</v-stepper-step>
+
+			<v-stepper-content step="2">
+				<v-card color="grey lighten-1" class="mb-5">
+					<app-stage-two :portfolioId="portfolioId"></app-stage-two>
+				</v-card>
+			</v-stepper-content>
+		</v-stepper>
+	</div>
+</template>
+
+<script>
+	import PortfolioService from '@/services/PortfolioService';
+	import appFormPanel from '../../universal/FormPanel.vue';
+	import appStageOne from './StageOne.vue';
+	import appStageTwo from './StageTwo.vue';
+
+	export default{
+		data(){
+			return {
+				user: null,
+				error: null,
+				portfolioId: null,
+				portfolioPictures: null,
+				stage: 1
+			}
+		},
+		async mounted(){
+			const username= this.$store.state.route.params.username;
+			if(this.$store.state.userLoggedIn){
+				this.user = this.$store.state.user;
+			} else {
+				this.$router.push({name: 'Portfolio', params: {username}});
+			}
+			
+			const userData = await PortfolioService.findPortfolio(username);
+			const portfolio = userData.data.portfolio;
+			console.log(portfolio)
+			//UNCOMMENT THIS WHEN DONE!!
+			/*if(portfolio.isSetUp){
+				this.$router.push({name: 'Portfolio', params: {username}});
+				return;
+			}*/
+			this.portfolioId = portfolio._id;
+			this.portfolioPictures = portfolio.profilePicture
+		},
+		methods:{
+			nextStage(){
+				this.stage++;
+			}
+		},
+		components:{
+			appStageOne,
+			appStageTwo
+		}
+	}
+</script>
+<style scoped>
+.error-alert{
+	text-align: center;
+	color: #dd1212;
+}
+#build-stage{
+	background-color: #fff;
+}
+
+</style>
---
+++ client/src/components/portfolio/build/StageOne.vue
@@ -0,0 +1,96 @@
+<template>
+	<app-form-panel title="Let's get to know you!" >
+		<form id="login-form">
+			<v-textarea label="About You" v-model="portfolioData.aboutUser"></v-textarea>
+			<br><br>
+			<v-layout wrap>
+				<v-flex xs12 sm6>
+					<v-btn @click="openUploadModal({type: 'portfolioImage', _id: portfolioId})">Add Portfolio Image</v-btn>
+				</v-flex>
+				<v-flex xs12 sm6 v-if="this.profilePicturePath">
+					<v-badge color="green">
+						<span slot="badge"><v-icon dark>done</v-icon></span>
+						<v-avatar  :size="100" color="grey lighten-4" >
+							<img :src="`http://localhost:8081/${profilePicturePath}`" alt="avatar">
+						</v-avatar>
+					</v-badge>
+				</v-flex>
+			</v-layout>
+		</form>
+		<br>
+		<div v-if="error" class="error-alert">{{ error }}</div>
+		<br>
+		<v-btn dark color="primary" @click="completeStageOne">Continue</v-btn>
+		<v-dialog v-model="uploadModal" width="500" >
+			<app-file-uploader :uploadTarget="uploadTarget" ref="fileUploadComponent" v-on:close="closeUploadModal"></app-file-uploader>
+		</v-dialog>
+	</app-form-panel>
+</template>
+
+<script>
+	import appFormPanel from '../../universal/FormPanel.vue';
+	import PortfolioService from '@/services/PortfolioService.js';
+	import appFileUploader from '../../universal/FileUploader.vue';
+	export default{
+		props: ['portfolioId', 'profilePictures'],
+		data(){
+			return {
+				portfolioData:{
+					aboutUser: null,
+					profilePicture: null,
+				},
+				profilePicturePath: null,
+				error: null,
+				uploadModal: false,
+				uploadTarget: null
+			}
+		},
+		components:{
+			appFormPanel,
+			appFileUploader
+		},
+		watch:{
+			uploadModal(){
+				if(!this.uploadModal){
+					this.uploadTarget = null;
+					this.$refs.fileUploadComponent.clearUploader();
+				}
+			}
+		},
+		methods: {
+			async completeStageOne(){
+				try{
+					const portfolioUpdate = await PortfolioService.updatePortfolio(this.portfolioId, this.portfolioData);
+					console.log(portfolioUpdate);
+					if(!portfolioUpdate.data.ok){
+						this.error = "Error updating portfolio";
+						return;
+					} else {
+						this.$emit('next');
+					}
+				}catch(error){
+					console.log("ERROR", error);
+					this.error = error;
+				}
+			},
+			openUploadModal(targetData){
+				targetData.currentPictures = [];
+				console.log(targetData);
+				this.uploadTarget = targetData
+				this.uploadModal = true;
+			},
+			closeUploadModal(data){
+				console.log("YOUR DATA SIR", data);
+				this.profilePicturePath = data.filePath;
+				this.uploadModal = false;
+			}
+		}
+	}
+</script>
+<style scoped>
+.error-alert{
+	text-align: center;
+	color: #dd1212;
+}
+
+</style>
