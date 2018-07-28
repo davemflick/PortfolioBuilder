d6dd4a3e71382a2e2e3fb6b7bb8dfd2e2c8060c7
davemflick <davemflick@yahoo.com>
2018-07-28 17:51:26 -0400

    a few style additions and submit button

--- client/src/components/portfolio/edit/Certs.vue
+++ client/src/components/portfolio/edit/Certs.vue
@@ -1,32 +1,44 @@
 <template>
   <app-form-panel title="Certifications" v-if="portfolio">
-   <v-layout wrap class="my-2">
-    <v-flex xs12 sm6 px-1>
-      <v-text-field type="text" label="Name" v-model="newCert.name"></v-text-field>
-    </v-flex>
-    <v-flex xs12 sm6 px-1>
-      <v-text-field type="text" label="Date Obtained" v-model="newCert.dateObtained" @click="dateModal = true"></v-text-field>
-      <v-dialog ref="dialog" v-model="dateModal" persistent lazy full-width width="290px" >
-      <v-date-picker v-model="newCert.dateObtained" scrollable>
-          <v-spacer></v-spacer>
-          <v-btn flat color="primary" @click="dateModal = false; newCert.dateObtained = null">Remove</v-btn>
-          <v-btn flat color="primary" @click="dateModal = false">Set</v-btn>
-        </v-date-picker>
-    </v-dialog>
-    </v-flex>
-    <v-flex xs12 px-1>
-      <v-textarea type="text" 
-      label="Description" 
-      v-model="newCert.about"
-      ></v-textarea>
-    </v-flex>
-    <v-flex xs4 sm2>
-      <v-btn @click="openUploadModal({type: 'certification', _id: portfolio._id})">Add Image</v-btn>
-    </v-flex>
-    <v-flex v-if="portfolio.certifications.length === 0" xs8 sm10>
-      <p class="text-lg-right"> No certifications </p>
-    </v-flex>
-  </v-layout>
+    <br> <br>
+    <h4>Current Certifications</h4>
+    <br>
+    <p v-show="portfolio.certifications.length === 0">You have no certifications</p>
+    <br> <br>
+    <h4>Add New Certification</h4>
+    <br>
+    <div class="new-cert pa-2">
+     <v-layout wrap class="my-2">
+      <v-flex xs12 sm6 px-1>
+        <v-text-field type="text" label="Name" v-model="newCert.name"></v-text-field>
+      </v-flex>
+      <v-flex xs12 sm6 px-1>
+        <v-text-field type="text" label="Date Obtained" v-model="newCert.dateObtained" @click="dateModal = true"></v-text-field>
+        <v-dialog ref="dialog" v-model="dateModal" persistent lazy full-width width="290px" >
+          <v-date-picker v-model="newCert.dateObtained" scrollable>
+            <v-spacer></v-spacer>
+            <v-btn flat color="primary" @click="dateModal = false; newCert.dateObtained = null">Remove</v-btn>
+            <v-btn flat color="primary" @click="dateModal = false">Set</v-btn>
+          </v-date-picker>
+        </v-dialog>
+      </v-flex>
+      <v-flex xs12 px-1>
+        <v-textarea type="text" 
+        label="Description" 
+        v-model="newCert.about"
+        ></v-textarea>
+      </v-flex>
+      <v-flex xs4 sm2>
+        <v-btn @click="openUploadModal({type: 'certification', _id: portfolio._id})">Add Image</v-btn>
+      </v-flex>
+      <v-flex v-if="!newCert.picturePath" xs8 sm10>
+        <p class="text-lg-right">No Certification Image</p>
+      </v-flex>
+      <v-flex xs12 class="py-1">
+        <v-btn class="primary" @click="submitNewCert">Add Certification</v-btn>
+      </v-flex>
+    </v-layout>
+  </div>
   <v-dialog v-model="uploadModal" width="500" >
     <app-file-uploader :uploadTarget="uploadTarget" ref="fileUploadComponent" v-on:close="closeUploadModal"></app-file-uploader>
   </v-dialog>
@@ -58,7 +70,14 @@
       }
     },
     methods:{
-     openUploadModal(targetData){
+     async submitNewCert(){
+      let cert = JSON.parse(JSON.stringify(this.newCert));
+      // if(cert.dateObtained){
+      //   cert.dateObtained = new Date(cert.dateObtained);
+      // }
+      console.log(cert.dateObtained);
+    },
+    openUploadModal(targetData){
       console.log(targetData);
       this.uploadTarget = targetData
       this.uploadModal = true;
@@ -73,5 +92,8 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.new-cert{
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
 </style>
