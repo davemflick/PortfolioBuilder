46342519f60cd1a88f7088e9b9af2191535afbe1
davemflick <davemflick@yahoo.com>
2018-07-29 13:55:04 -0400

    clean up

--- client/src/main.js
+++ client/src/main.js
@@ -7,14 +7,16 @@ import 'vuetify/dist/vuetify.min.css';
 import store from './store/store.js';
 import VueDraggableResizable from 'vue-draggable-resizable'
 import Devicon from 'devicon';
+
+
  
-Vue.component('vue-draggable-resizable', VueDraggableResizable)
+Vue.component('vue-draggable-resizable', VueDraggableResizable);
 
 Vue.config.productionTip = false
 Vue.use(Devicon);
 Vue.use(Vuetify);
 sync(store, router);
-//sup
+
 new Vue({
   el: '#app',
   router,
@@ -22,3 +24,4 @@ new Vue({
   components: { App },
   template: '<App/>'
 })
+
