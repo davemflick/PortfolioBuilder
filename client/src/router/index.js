f9f17b7b86e5c67e71d1f12173c5884dea862a91
davemflick <davemflick@yahoo.com>
2018-08-04 07:27:47 -0400

    add beforeEach function to check auth token

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -7,46 +7,72 @@ import Portfolio from '@/components/portfolio/Portfolio.vue'
 import PortfolioBuild from '@/components/portfolio/build/PortfolioBuild.vue'
 import EditPortfolio from '@/components/portfolio/edit/Portfolio.vue'
 import FileUploader from '@/components/universal/FileUploader.vue'
+import AuthService from '../services/AuthenticationService.js';
+import store from '../store/store.js';
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
 	mode: "history",
   routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
-    },
-     {
-      path: '/login',
-      name: 'Login',
-      component: Login
-    },
-    {
-      path: '/signup',
-      name: 'SignUp',
-      component: SignUp
-    },
-    {
-      path: '/portfolio/:username?',
-      name: 'Portfolio',
-      component: Portfolio
-    },
-    {
-      path: '/portfolio/:username/build',
-      name: 'PortfolioBuild',
-      component: PortfolioBuild
-    },
-    {
-      path: '/portfolio/:username/edit',
-      name: 'EditPortfolio',
-      component: EditPortfolio
-    },
-    {
-      path: '/file-uploader',
-      name: 'FileUploader',
-      component: FileUploader
-    }
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/signup',
+    name: 'SignUp',
+    component: SignUp
+  },
+  {
+    path: '/portfolio/:username?',
+    name: 'Portfolio',
+    component: Portfolio
+  },
+  {
+    path: '/portfolio/:username/build',
+    name: 'PortfolioBuild',
+    component: PortfolioBuild
+  },
+  {
+    path: '/portfolio/:username/edit',
+    name: 'EditPortfolio',
+    component: EditPortfolio
+  },
+  {
+    path: '/file-uploader',
+    name: 'FileUploader',
+    component: FileUploader
+  }
   ]
+});
+
+router.beforeEach(async (to, from, next)=>{
+  //check if user token is still vaild, if not, remove user from state
+  if(store.state.token){
+    try{
+      const tokenIsValid = await AuthService.checkToken(store.state.token);
+      if(!tokenIsValid.data.ok){
+        store.dispatch('setUser', null);
+        store.dispatch('setToken', null);
+        next();
+      }
+    } catch(error){
+      console.log("ERROR in checking token expiration");
+      next();
+    }
+  } else {
+    next();
+  }
+  
 })
+
+export default router
+
+
