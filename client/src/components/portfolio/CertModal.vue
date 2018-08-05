240fe082f79f78cccb8e7afb16584a351a6c9443
davemflick <davemflick@yahoo.com>
2018-08-05 13:18:48 -0400

    Modal to show off cert

---
+++ client/src/components/portfolio/CertModal.vue
@@ -0,0 +1,64 @@
+<template>
+	<v-layout row justify-center v-if="cert">
+		<v-dialog max-width="600px" :value="isOpen" @input="$emit('close')">
+			<v-card>
+				<v-card-title>
+					<span class="headline">Certification</span>
+				</v-card-title>
+				<div class="pa-2 text-xs-center">
+					<img v-if="cert.picturePath" :src="`http://localhost:8081/${cert.picturePath}`" class="img-responsive" />
+					<img v-else :src="defaultCert" class="img-responsive" />
+					<h3>
+						{{ cert.name }}
+					</h3>
+					<p v-if="cert.dateObtained"><em>{{ formatDate(cert.dateObtained) }}</em></p>
+					<p v-if="cert.about">{{ cert.about }} </p>
+				</div>
+				<v-card-actions>
+					<v-spacer></v-spacer>
+					<v-btn color="blue darken-1" flat @click="$emit('close')">Close</v-btn>
+				</v-card-actions>
+			</v-card>
+		</v-dialog>
+	</v-layout>
+</template>
+
+<script>
+	import defaultCert from '@/assets/images/default-cert.png';
+	import moment from 'moment';
+
+	export default{
+		props: ['cert', 'isOpen'],
+		data(){
+			return{
+				defaultCert
+			}
+		},
+		watch:{
+			isOpen(event){
+				console.log(event)
+				if(!this.isOpen){
+					console.log('fucker')
+					this.$emit('close');
+				}
+			}
+		},
+		methods:{
+			formatDate(date){
+				return moment(date).format('MM/DD/YYYY');
+			}
+		}
+	}
+
+</script>
+
+<style scoped>
+.single-cert>div{
+	border: 1px solid #eee;
+	border-radius: 2px;
+	cursor: pointer;
+}
+</style>
+
+
+
