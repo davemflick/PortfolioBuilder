71b722eaae157ce69c3903623d4c74be64fbbfb8
davemflick <davemflick@yahoo.com>
2018-08-05 13:17:55 -0400

    remove v-modal and use :value and input event on modal

--- client/src/components/portfolio/ProjectModal.vue
+++ client/src/components/portfolio/ProjectModal.vue
@@ -1,6 +1,6 @@
 <template>
 	<v-layout row justify-center>
-		<v-dialog v-model="isOpen" fullscreen hide-overlay transition="dialog-bottom-transition">
+		<v-dialog :value="isOpen" @input="$emit('close')" fullscreen hide-overlay transition="dialog-bottom-transition">
 			<v-card>
 				<v-toolbar dark color="primary">
 					<v-btn icon dark @click="$emit('close')">
