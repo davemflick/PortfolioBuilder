23fcdbb6af6d719c8ee12d1b3bcd7a4f3903803e
davemflick <davemflick@yahoo.com>
2018-08-05 11:46:49 -0400

    pull in all of users skills and produce devicons

---
+++ client/src/components/portfolio/AllSkills.vue
@@ -0,0 +1,26 @@
+<template>
+	<v-flex xs12 v-if="skills.length > 0">
+		<h2>Skills</h2>
+		<v-layout wrap>
+			<v-flex xs6 sm4 md3 
+			class="pa-2 text-xs-center"
+			v-for="(skill, i) in skills"
+			:key="`skill-${i}`">
+			<div class="pa-2">
+				<i :class="`devicon-icon devicon-${skill}`"></i>
+			</div>
+		</v-flex>
+	</v-layout>
+</v-flex>
+</template>
+
+<script>
+	export default{
+		props: ['skills']
+	}
+
+</script>
+
+<style scoped>
+
+</style>
