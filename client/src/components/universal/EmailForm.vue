248587eeb27087b0445b259a127a8398600181da
davemflick <davemflick@yahoo.com>
2018-08-10 16:41:27 -0400

    component for sending emails

---
+++ client/src/components/universal/EmailForm.vue
@@ -0,0 +1,31 @@
+<template>
+	<v-layout>
+		<v-flex xs12 sm6 class="pa-1">
+			<v-text-field  v-model="emailMsg.from" label="From" required ></v-text-field>
+		</v-flex>
+		<v-flex xs12 sm6 class="pa-1">
+			<v-text-field  v-model="emailMsg.about" label="About" required ></v-text-field>
+		</v-flex>
+		<v-flex xs12 sm6 class="pa-1">
+			<v-textarea  v-model="emailMsg.msg" label="Message" required ></v-textarea>
+		</v-flex>
+	</v-layout>
+</template>
+
+<script>
+export default{
+	data(){
+		return{
+			emailMsg: {
+				from: null,
+				about: null,
+				message: null
+			}
+		}
+	}
+}
+</script>
+<style scoped>
+
+
+</style>
