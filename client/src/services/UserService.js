1c6c7da0d0f99293591692423639cd2c3817a3d1
davemflick <davemflick@yahoo.com>
2018-08-12 12:58:13 -0400

    endpoint to send email

--- client/src/services/UserService.js
+++ client/src/services/UserService.js
@@ -3,5 +3,9 @@ import Api from '@/services/Api';
 export default {
 	updateUserInfo(req){
 		return Api().put(`user/update`, req)// Get user
+	},
+
+	sendUserEmail(req){
+		return Api().post('/mailer/send', req)
 	}
 }
