f9493aa4946f8abbd9091da1a43244633cd63d99
davemflick <davemflick@yahoo.com>
2018-08-13 19:11:19 -0400

    description of porteloper

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,51 +1,65 @@
 <template>
-  <section id="home-head">
-    <v-container container>
-      <h1 class="text-xs-center">
-        Porteloper
-      </h1>
-      <h3 class="text-xs-center mb-5">
-        Portfolios for Devlopers
-      </h3>
-      <v-layout class="my-4" wrap>
-        <v-flex xs12 sm6 md4 class="pa-2 text-xs-center">
-          <div class="hp-block">
-            <v-icon class="hp-icon pa-3" color="primary">person_add</v-icon>
-            <h4 class="pa-3">
-              Create an Account
-            </h4>
-          </div>
-        </v-flex>
-        <v-flex xs12 sm6 md4 class="pa-2 text-xs-center">
-          <div class="hp-block">
-            <v-icon class="hp-icon pa-3" color="primary">build</v-icon>
-            <h4 class="pa-3">
-              Build your Portfolio
-            </h4>
-          </div>
-        </v-flex>
-        <v-flex xs12 sm6 md4 class="pa-2 text-xs-center">
-          <div class="hp-block">
-            <v-icon class="hp-icon pa-3" color="primary">public</v-icon>
-            <h4 class="pa-3">
-              Share your Work
-            </h4>
-          </div>
-        </v-flex>
-      </v-layout>
-    </v-container>
-  </section>
+  <div>
+    <section id="home-head">
+      <v-container container>
+        <h1 class="text-xs-center">
+          Porteloper
+        </h1>
+        <h3 class="text-xs-center mb-5">
+          Portfolios for Devlopers
+        </h3>
+        <v-layout class="my-4" wrap>
+          <v-flex xs12 sm6 md4 class="pa-2 text-xs-center">
+            <div class="hp-block">
+              <v-icon class="hp-icon pa-3" color="primary">person_add</v-icon>
+              <h4 class="pa-3">
+                Create an Account
+              </h4>
+            </div>
+          </v-flex>
+          <v-flex xs12 sm6 md4 class="pa-2 text-xs-center">
+            <div class="hp-block">
+              <v-icon class="hp-icon pa-3" color="primary">build</v-icon>
+              <h4 class="pa-3">
+                Build your Portfolio
+              </h4>
+            </div>
+          </v-flex>
+          <v-flex xs12 sm6 md4 class="pa-2 text-xs-center">
+            <div class="hp-block">
+              <v-icon class="hp-icon pa-3" color="primary">public</v-icon>
+              <h4 class="pa-3">
+                Share your Work
+              </h4>
+            </div>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </section>
+    <section class="pa-5 text-xs-center" id="about-section">
+      <v-container container>
+        <h2>What is Porteloper?</h2>
+        <h3 class="mb-4 mt-2">
+        Porteloper is a website designed to provide developers with a free, quick, and easy way to show off their work and accomplishments.
+        </h3>
+        <h2>Why use Porteloper?</h2>
+        <h3 class="mb-4 mt-2">
+        Don't have a place to show off your work? Is your current portfolio is a bit outdated and / or dificult to maintain? Don't have the time to build your own site from scratch? Whatever the reason, Porteloper is here to help. Just create an account, build your portfolio, and share it with the world!
+        </h3>
+      </v-container>
+    </section>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -64,7 +78,21 @@ h3{
   background: url('../assets/images/home-background.png') no-repeat;
   background-position: center center;
   background-size: cover;
-  min-height: 800px;
+  min-height: 680px;
+}
+
+#about-section{
+  background-color: #fff;
+}
+
+#about-section h2{
+  font-size: 2.5em;
+  color: #444;
+}
+
+#about-section h3{
+  font-size: 1.5em;
+  color: #444;
 }
 
 .hp-block{
