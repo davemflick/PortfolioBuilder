95a7815cd9a68447ac8b3a30ba9dbdc47ebe9b05
davemflick <davemflick@yahoo.com>
2018-08-13 18:39:32 -0400

    added some intro blocks

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -4,9 +4,35 @@
       <h1 class="text-xs-center">
         Porteloper
       </h1>
-      <h3 class="text-xs-center">
+      <h3 class="text-xs-center mb-5">
         Portfolios for Devlopers
       </h3>
+      <v-layout class="my-4" wrap>
+        <v-flex xs12 sm6 md4 class="pa-2 text-xs-center">
+          <div class="hp-block">
+            <v-icon class="hp-icon pa-3" color="primary">person_add</v-icon>
+            <h4 class="pa-3">
+              Create an Account
+            </h4>
+          </div>
+        </v-flex>
+        <v-flex xs12 sm6 md4 class="pa-2 text-xs-center">
+          <div class="hp-block">
+            <v-icon class="hp-icon pa-3" color="primary">build</v-icon>
+            <h4 class="pa-3">
+              Build your Portfolio
+            </h4>
+          </div>
+        </v-flex>
+        <v-flex xs12 sm6 md4 class="pa-2 text-xs-center">
+          <div class="hp-block">
+            <v-icon class="hp-icon pa-3" color="primary">public</v-icon>
+            <h4 class="pa-3">
+              Share your Work
+            </h4>
+          </div>
+        </v-flex>
+      </v-layout>
     </v-container>
   </section>
 </template>
@@ -39,6 +65,21 @@ h3{
   background-position: center center;
   background-size: cover;
   min-height: 800px;
+}
+
+.hp-block{
+  min-height: 250px;
+  padding: 10px;
+  background-color: #fff;
+  box-shadow: 0px 11px 15px -7px rgba(0,0,0,0.2), 0px 24px 38px 3px rgba(0,0,0,0.14), 0px 9px 46px 8px rgba(0,0,0,0.12);
+}
+
+.hp-icon{
+  font-size: 9em;
+}
+
+.hp-block h4{
+  font-size: 1.33em;
 }
 
 @media screen and (max-width: 600px){
