263bc967582d304ddba232001d5ee4ec24a4f88b
davemflick <davemflick@yahoo.com>
2018-08-14 18:43:46 -0400

    include db environmental variable

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,6 +1,9 @@
 
+require('dotenv').config();
+
 module.exports = {
 	port: process.env.PORT || 8081,
+	db: process.env.PORTELOPER_DB || "mongodb://localhost:27017/portbuilder",
 	authentication:{
 		jwtSecret: process.env.JWT_SECRET || 'secretboy buddy'
 	}
