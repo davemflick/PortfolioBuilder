5779f2d542c526b5d0e5f4545d1cfe397fc27d6f
davemflick <davemflick@yahoo.com>
2018-08-14 16:59:32 -0400

    only one link menu

--- client/src/components/universal/Header.vue
+++ client/src/components/universal/Header.vue
@@ -1,26 +1,14 @@
 <template>
 	<div>
 		<v-toolbar dark color="primary">
-			<v-menu :nudge-width="150" offset-y >
-				<v-toolbar-title class="pl-2" slot="activator">
-					<v-toolbar-side-icon></v-toolbar-side-icon>
-				</v-toolbar-title>
-				<v-list>
-					<v-list-tile v-for="item in items" :key="item" @click="">
-						<v-list-tile-title v-text="item"></v-list-tile-title>
-					</v-list-tile>
-				</v-list>
-			</v-menu>
 			<v-toolbar-title class="white--text">
 				<router-link :to="{name: 'Home'}" tag="span" class="hovlink">Porteloper</router-link>
 			</v-toolbar-title>
 			<v-spacer></v-spacer>
 			<v-text-field class="nav-search" color="#fff" prepend-icon="search" hide-details single-line></v-text-field>
-			<v-menu :nudge-width="150"  offset-y class="hidden-sm-and-down">
+			<v-menu :nudge-width="150" offset-y >
 				<v-toolbar-title class="pr-2" slot="activator">
-					<v-btn icon>
-						<v-icon>more_vert</v-icon>
-					</v-btn>
+					<v-toolbar-side-icon></v-toolbar-side-icon>
 				</v-toolbar-title>
 				<v-list>
 					<v-list-tile v-if="!this.$store.state.userLoggedIn">
