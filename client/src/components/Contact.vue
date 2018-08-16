2f47b0e8955fb0fd1fed9cf95131eaa6c87f22e9
davemflick <davemflick@yahoo.com>
2018-08-16 17:27:15 -0400

    this will be the contact page

---
+++ client/src/components/Contact.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <h1 class="text-xs-center">
+      Contact Us
+    </h1>
+  </v-container>
+</template>
+
+<script>
+  
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
