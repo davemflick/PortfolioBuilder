9fe20a707cb08fda9a83ab25e5a32bb67d09daa7
davemflick <davemflick@yahoo.com>
2018-08-17 16:13:43 -0400

    edit some text

--- server/src/emailTemplates/general.js
+++ server/src/emailTemplates/general.js
@@ -78,7 +78,7 @@ module.exports = {
 		<td style="font-family: sans-serif; font-size: 15px; vertical-align: top;">&nbsp;</td>
 		<td class="container" style="font-family: sans-serif; font-size: 15px; vertical-align: top; display: block; Margin: 0 auto; max-width: 580px; padding: 10px; width: 580px;">
 		<div class="content" style="box-sizing: border-box; display: block; Margin: 0 auto; max-width: 600px; padding: 10px;">
-		<span class="preheader" style="color: transparent; display: none; height: 0; max-height: 0; max-width: 0; opacity: 0; overflow: hidden; mso-hide: all; visibility: hidden; width: 0;">This is preheader text. Some clients will show this text as a preview.</span>
+		<span class="preheader" style="color: transparent; display: none; height: 0; max-height: 0; max-width: 0; opacity: 0; overflow: hidden; mso-hide: all; visibility: hidden; width: 0;">Porteloper</span>
 		<table class="main" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; background: #ffffff; border-radius: 3px;">
 		<tr>
 		<td class="wrapper" style="font-family: sans-serif; font-size: 15px; vertical-align: top; box-sizing: border-box; padding: 20px;">
