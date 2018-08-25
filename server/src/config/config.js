d14130e38ce2d04ce06d01624ee7e203924b4f23
davemflick <davemflick@yahoo.com>
2018-08-25 15:51:05 -0400

    bad syntax fix

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 module.exports = {
 	port: process.env.PORT || 8081,
 	db: process.env.PORTELOPER_DB || "mongodb://localhost:27017/portbuilder",
-	sessionSecret: process.env.SESSION_SECRET || 'my little secret'
+	sessionSecret: process.env.SESSION_SECRET || 'my little secret',
 	authentication:{
 		jwtSecret: process.env.JWT_SECRET || 'secretboy buddy'
 	}
