66122327555427eb905f43b7da7460d3b53abbfb
davemflick <davemflick@yahoo.com>
2018-08-29 18:06:10 -0400

    regex for url validation was still off, should be better

--- client/src/components/portfolio/edit/PortfolioForm.vue
+++ client/src/components/portfolio/edit/PortfolioForm.vue
@@ -77,7 +77,7 @@
       validate(type, value){
         const validations = {
           url: {
-            re: /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#-%.]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/,
+            re: /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+\.[a-z]+(\/[a-zA-Z0-9-.%_#]+\/?)*$/,
             required: false
           }
         }
